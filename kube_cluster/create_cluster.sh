582f6ef5e99755c75c51632d1805aa48dcbb8ee9
tpk@tb1 <admin@reverie.tech>
2020-04-02 12:05:17 +0100

    more eksctl config

--- kube_cluster/create_cluster.sh
+++ kube_cluster/create_cluster.sh
@@ -1,7 +1,8 @@
 eksctl create cluster \
 --name=big-cheese \
 --region=us-west-2 \
+--kubeconfig=/var/lib/jenkins/.kube/config \
+--nodes=2 \
+--node-type= t2.micro \
 --nodes-min=2 \
---nodes-max=5 \
---node-type=t2.micro \
---kubeconfig=/var/lib/jenkins/.kube/config
+--nodes-max=5
